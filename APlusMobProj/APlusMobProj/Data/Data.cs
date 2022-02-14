5a94a608444f108b7de175d2dcdba224b056654a
Егор Артемьев <batman_batman_1910@mail.ru>
2022-02-14 21:39:09 +0300

    edit somethings

---
+++ APlusMobProj/APlusMobProj/Data/Data.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using APlusMobProj.Models;
+
+namespace APlusMobProj.Data
+{
+	class Data
+	{
+        readonly SQLiteConnection db;
+        public Data(string connectionString)
+        {
+            db = new SQLiteConnection(connectionString);
+            db.CreateTable<AplusModel>();
+        }
+        public IEnumerable<AplusModel> GetProject()
+        {
+            return db.Table<AplusModel>().ToList();
+        }
+        public AplusModel GetProject(int id)
+        {
+            return db.Table<AplusModel>()
+                                .Where(i => i.ID == id)
+                                .FirstOrDefault();
+        }
+        public int SaveProject(AplusModel project)
+        {
+            if (project.ID != 0)
+            {
+                return db.Update(project);
+            }
+            else
+            {
+                return db.Insert(project);
+            }
+        }
+        public int DeleteProject(int id)
+        {
+            return db.Delete<AplusModel>(id);
+        }
+    }
+}
--- APlusMobProj/APlusMobProj/Models/AplusModel.cs
+++ APlusMobProj/APlusMobProj/Models/AplusModel.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SQLite;
 
 namespace APlusMobProj.Models
 {
+	[Table("Projects")]
 	class AplusModel
 	{
-	}
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int ID { get; set; }
+        [Unique]
+        public string NameProject { get; set; }
+        public string Description { get; set; }
+        public string NumberPhone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+    }
 }

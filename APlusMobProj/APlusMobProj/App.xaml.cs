c6ef8d15f01144e3ca4b4ec744440efeb621f15d
Егор Артемьев <batman_batman_1910@mail.ru>
2022-02-14 22:31:55 +0300

    broke

--- APlusMobProj/APlusMobProj/App.xaml.cs
+++ APlusMobProj/APlusMobProj/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using APlusMobProj.Date;
+using System.IO;
 
 [assembly: ExportFont("MaterialIcons-Regular.ttf", Alias = "AplusFont")]
 
@@ -8,6 +10,18 @@ namespace APlusMobProj
 {
 	public partial class App : Application
 	{
+		static Data data;
+		public static Data data
+		{
+			get
+			{
+				if (data == null)
+				{
+					data = new Data(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Aplusbd.db3"));
+				}
+				return data;
+			}
+		}
 		public App()
 		{
 			InitializeComponent();
